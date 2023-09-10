16f00e8b85e59acbfdc2bb9058a2312311ce5634
TravelTec <59958315+TravelTec@users.noreply.github.com>
2023-09-10 16:40:07 -0300

    Update tarifario.php

--- tarifario.php
+++ tarifario.php
@@ -72,8 +72,44 @@ License: GPLv2
 
  
 require_once plugin_dir_path(__FILE__) . 'includes/tarifario-functions.php';
+require_once 'plugin-update-checker-4.10/plugin-update-checker.php'; 
 
 
+/*
+
+
+
+ * Plugin Update Checker Setting
+
+
+
+ *
+
+
+
+ * @see https://github.com/YahnisElsts/plugin-update-checker for more details.
+
+
+
+ */
+
+
+
+function shortcode_puc_tarifario() {
+    if ( ! is_admin() || ! class_exists( 'Puc_v4_Factory' ) ) { 
+        return; 
+    } 
+
+    $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker( 
+        'https://github.com/TravelTec/TT-Tarifario', 
+        __FILE__, 
+        'TT-Tarifario' 
+    ); 
+
+    // (Opcional) Set the branch that contains the stable release. 
+    $myUpdateChecker->setBranch('main');
+}
+add_action( 'admin_init', 'shortcode_puc_tarifario' );
 
 
 
